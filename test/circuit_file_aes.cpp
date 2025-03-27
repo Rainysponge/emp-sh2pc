09111974de761a7659ad3538cf00d49c70b62956
rainysponge <375364412@qq.com>
2025-03-27 14:33:40 +0800

    format

--- test/circuit_file_aes.cpp
+++ test/circuit_file_aes.cpp
@@ -4,45 +4,50 @@ using namespace std;
 const string circuit_file_location = macro_xstr(EMP_CIRCUIT_PATH);
 
 int port, party;
-string file = circuit_file_location+"/bristol_fashion/aes_128.txt";
+string file = circuit_file_location + "/bristol_fashion/aes_128.txt";
 
-
-vector<Bit> cat_vector(vector<Bit> key, vector<Bit> plaintext){
+vector<Bit> cat_vector(vector<Bit> key, vector<Bit> plaintext)
+{
 	vector<Bit> result = key;
-    result.insert(result.end(), plaintext.begin(), plaintext.end());
-    return result;
+	result.insert(result.end(), plaintext.begin(), plaintext.end());
+	return result;
 }
 
-string bits2string(vector<Bit> bits){
+string bits2string(vector<Bit> bits)
+{
 	string result;
-    for (const Bit &b : bits) {
-        result += b.reveal<bool>() ? '1' : '0';
-    }
-    return result;
+	for (const Bit &b : bits)
+	{
+		result += b.reveal<bool>() ? '1' : '0';
+	}
+	return result;
 }
 
-string reverse_string(string str){
+string reverse_string(string str)
+{
 	reverse(str.begin(), str.end());
-    return str;
+	return str;
 }
 
-void test() {
+void test()
+{
 	std::srand(static_cast<unsigned int>(
-        std::chrono::system_clock::now().time_since_epoch().count()
-    ));
+		std::chrono::system_clock::now().time_since_epoch().count()));
 	auto start1 = clock_start();
 	BristolFashion cf(file.c_str());
 	cout << "Time for Reading File and Creating Circuits: " << time_from(start1) << endl;
-	
-	vector<Bit> key(128);
-    for (auto& bit : key) {
-        bit = (rand() % 2) == 1;
-    }
 
-    vector<Bit> plaintext(128);
-    for (auto& bit : plaintext) {
-        bit = (rand() % 2) == 1;
-    }
+	vector<Bit> key(128);
+	for (auto &bit : key)
+	{
+		bit = (rand() % 2) == 1;
+	}
+
+	vector<Bit> plaintext(128);
+	for (auto &bit : plaintext)
+	{
+		bit = (rand() % 2) == 1;
+	}
 	cout << "key      : " << bits2string(key) << endl;
 	cout << "plaintext: " << bits2string(plaintext) << endl;
 
@@ -51,13 +56,14 @@ void test() {
 
 	Integer c(128, 1, PUBLIC);
 	auto start2 = clock_start();
-	cf.compute((block*)c.bits.data(), (block*)a.bits.data());
-	cout << "ciphertext: "<<reverse_string(c.reveal<string>())<<endl;
+	cf.compute((block *)c.bits.data(), (block *)a.bits.data());
+	cout << "ciphertext: " << reverse_string(c.reveal<string>()) << endl;
 	cout << "Time for Computation: " << time_from(start2) << endl;
 }
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	parse_party_and_port(argv, &party, &port);
-	NetIO* io = new NetIO(party==ALICE?nullptr:"127.0.0.1", port);
+	NetIO *io = new NetIO(party == ALICE ? nullptr : "127.0.0.1", port);
 
 	setup_semi_honest(io, party);
 	test();
